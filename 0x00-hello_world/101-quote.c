37cc662bb2eaeb3c894348aab44a058668531b5b
Mumeeee <muminatadekunle@gmail.com>
2022-09-08 21:24:22 -0700

    prints exact script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,20 @@
-nclude <stdio.h>
+/*
+ *  * File: 101-quote.c
+ *   */
+
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *   *                followed by a new line, to standard error.
+ *    *
+ *     * Return: Always 1.
+ *      */
 int main(void)
 {
-			write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-						return (1);
+		write(2,
+					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+					      	      59);
+
+			return (1);
 }
