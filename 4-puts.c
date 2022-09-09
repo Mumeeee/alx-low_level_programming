f8380be73973d6490a3849d9b5d84673a59fc50c
Mumeeee <muminatadekunle@gmail.com>
2022-09-08 21:12:49 -0700

    adds

--- 4-puts.c
+++ 4-puts.c
@@ -1,10 +1,8 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
