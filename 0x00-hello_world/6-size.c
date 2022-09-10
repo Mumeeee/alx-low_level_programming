cbc0e4d2ccbd61b66232123e669509da17efe5f4
Mumeeee <muminatadekunle@gmail.com>
2022-09-10 10:36:42 -0700

    alx task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- *  * main - Prints the size of various types based on
- *   *        the computer it is compiled and run on..
- *    *
- *     * Return: Always 0.
- *      */
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
-			printf("Size of an int: %zu byte(s)\n", sizeof(int));
-				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
-							return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
