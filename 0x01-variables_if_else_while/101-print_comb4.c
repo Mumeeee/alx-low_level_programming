206c551f2b833c95256556dcd620b9587bc795ed
Mumeeee <muminatadekunle@gmail.com>
2022-09-10 05:10:10 -0700

    m

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,20 +14,19 @@ int main(void)
 			{
 				for (l = 50; l < 58; l++)
 				{
-					if (l > m && m > n)
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
 					{
-						putchar(n);
-						putchar(m);
-						putchar(l);
-						if (n != 55 || m != 56)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
 		}
-		putchar('\n');
-		return (0)	
 	}
+	putchar('\n');
+	return (0);
+}
+
