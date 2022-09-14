519388bc7855f7d0d2a6a2725359ed2095ce2761
Mumeeee <muminatadekunle@gmail.com>
2022-09-14 02:58:11 -0700

    task 0 alx c functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- */
-int main (void)
+ *  * main -  prints _putchar, followed by a new line
+ *   * Return: Always 0 (Success)
+ *    */
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -13,9 +12,9 @@ int main (void)
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar('a');	
+	_putchar('r');	
 	_putchar('\n');
 
-	return 0;
+											return (0);
 }
